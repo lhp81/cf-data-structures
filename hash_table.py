2dfbde7682f87c8ee8d1ebe3581513dd8022acc3
Luke Petschauer <lpetschauer@gmail.com>
2014-02-28 10:09:04 -0800

    continue work on hash_table & tests

--- hash_table.py
+++ hash_table.py
@@ -8,7 +8,7 @@ class NaiveHashTable(object):
     """A hash table."""
 
     def __init__(self, slots=10):  # length of 10 chosen b/c that's what we
-                                    # used in class. it can be any value.
+                                   # used in class. it can be any value.
         self.slots = slots
         self.bucket_list = Bucket()  # this is what will catch the k:v pairs
         for i in range(self.slots):   # put in values from 0-9. so, 10 buckets
@@ -19,30 +19,33 @@ class NaiveHashTable(object):
         key_to_get = hash_(key)
         pass
 
+        else:
+            raise KeyError("That key cannot be found in the hash. Bummer.")
+
     def set(self, key, val):
         """stores the given val using the given key"""
         bucket_number = self.hash_(key)
-        for i in range(1, bucket_number):
-        if not self.bucket_list.head:
+        if not self.bucket_list[bucket_number].head:
            self.bucket_list.head = bucket_number
            list_of_hashes = SingleLL()
            list_of_hashes.insert((key, val))
         else:
+            for i in range(0, len(self.bucket_list[bucket_number])):
+                if not 
             # look through the list until you find one that's empty.
             pass
 
-        raise KeyError("That key cannot be found in the hash. Bummer.")
 
     def hash_(self, key):
         """hashes the key provided"""
         if type(key) is not str:    # my ht can only deal with strings. naive.
             raise TypeError("This is a naive hash table and can only handle \
                 strings.")
-        else:                       # so, if it is a string...
-            adder = 0              # we take each character in the string
-            for char in key:        # and turn it into a value
-                adder += ord(char)     # these values are put in a list
-            return adder % self.slots  # and then added up and modulo'd.
+        else:                           # so, if it is a string...
+            adder = 0                   # we take each character in the string
+            for char in key:            # and turn it into a value
+                adder += ord(char)      # these values are put in a list
+            return adder % self.slots   # and then added up and modulo'd.
 
 
 class Bucket(SingleLL):
--- hash_tests.py
+++ hash_tests.py
@@ -1,22 +1,22 @@
 # tests for my hash table.
 
 import unittest
-from data_structures.hash_table import NaiveHashTable
+from hash_table import NaiveHashTable
 from linkedlist import Node, SingleLL  # do I need this?
 
 
 class HashTests(unittest.TestCase):
     """To see if the parts of my hash table work."""
 
-    def test_to_see_if_i_hash_keys_correctly(self):
+    def test_to_see_if_i_hash_keys_correctly(self):  # passes
         mh = NaiveHashTable()
-        self.assertEqual(mh.hash('hello'), 2)
+        self.assertEqual(mh.hash_('hello'), 2)
         # remember, it's a 2 here because we're taking 532 % 10
         # 532 = sum of ord() values of char in 'hello'; 10 = length.
 
-    def test_to_see_if_hash_handles_nonstring_keys_correctly(self):
+    def test_to_see_if_hash_handles_nonstring_keys_correctly(self):  #passes
         mh = NaiveHashTable()
-        self.assertRaises(TypeError, lambda: mh.hash(123))
+        self.assertRaises(TypeError, lambda: mh.hash_(123))
 
     def test_assigning_hash_to_a_bucket(self):
         mh = NaiveHashTable()
