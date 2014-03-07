dec20671ba2d90e4b8df60a64e838622e126ae61
Luke Petschauer <lpetschauer@gmail.com>
2014-03-07 14:41:23 -0800

    working function

---
+++ make_month.py
@@ -0,0 +1,22 @@
+from calendar import monthrange
+
+class MakeMonth(object):
+    """ Creates an object representing a calendar month in a given year.
+    You can then find out what day of the week any day of the month was/is/will
+    be.
+    """
+
+    def __init__(self, year, month):
+        self.month = month
+        self.year = year
+        self.weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                        'Friday', 'Saturday', 'Sunday']
+        # the above means 0=Monday, etc.
+        self.start_of_mnth, self.end_of_mnth = calendar.monthrange(year, month)
+
+    def day(self, day):
+        if day > self.end_of_mnth:
+            raise ValueError("The month isn't that long.")
+        else:
+            day_of_week = (self.start_of_mnth + (day - 1)) % 7
+            return self.weekdays[day_of_week]
