25bf59423ecd527b9a3005cfb85236693724c346
Luke Petschauer <lpetschauer@gmail.com>
2014-03-10 22:36:45 -0700

    update Binary Single Tree

--- bst.py
+++ bst.py
@@ -1,19 +1,12 @@
 import random
 
 
-class Node(object):
-    """ This is a node."""
-    def __init__(self, value=None):
-        self.value = value
-        self.left = None
-        self.right = None
-
-
 class BSTree(object):
     """This is a search tree"""
-    def __init__(self, value):
-        self.root = None
-        self.size = 0
+    def __init__(self, value=None):
+        self.value = value
+        self.right = None
+        self.left = None
 
     def check_rl(self, value):
         pass
@@ -22,46 +15,78 @@ class BSTree(object):
         """will insert the value val into the BST.  If val is already present,
         it will be ignored
         """
-        if self.root is None:
-            self.root = Node(value)
+        if self.value is None:
+            self.value = value
         else:
             if value > self.value:
-                self.right = Node(value)
+                if self.right is None:
+                    self.right = BSTree(value)
+                else:
+                    self.right.insert(value)
             if value < self.value:
-                self.left = Node(value)
+                if self.left is None:
+                    self.left = BSTree(value)
+                else:
+                    self.left.insert(value)
             else:
-                break
-        self.size += 1
+                pass
 
     def contains(self, val):
         """will return True if val is in the BST, False if not"""
-        pass
+        try:
+            if val == self.value:
+                return True
+            elif val > self.value:
+                return self.right.contains(val)
+            elif val < self.value:
+                return self.left.contains(val)
+        except AttributeError:
+            return False
 
     def size(self):
         """will return the integer size of the BST (equal to the total number
             of values stored in the tree), 0 if the tree is empty."""
-        return self.size
+        total = 0
+        if not self.value:
+            return total
+        else:
+            total += self.value
+            if self.right:
+                total += self.right.size()
+            if self.left:
+                total += self.left.size()
+        return total
 
     def depth(self):
         """will return an integer representing the total number of levels in
         the tree"""
-        pass
+        depth = 0
+        if not self.value:
+            return depth
+        else:
+            depth += 1
+            if (self.right and not self.left) or (self.right and :
 
     def balance(self):
-        """will return an integer, positive or negative that represents how
-        well-balanced the tree is. Trees which are higher on the left than the
-        right should return a positive value, trees which are higher on the
-        right than the left should return a negative value.  An
-        ideally-balanced tree should return 0.
+        """Will return an integer, positive or negative that represents how
+           well-balanced the tree is.
+        Trees which are higher on the left than the right should return a
+           positive value, trees which are higher on the right than the left
+           should return a negative value.
+        An ideally-balanced tree should return 0.
         """
-        pass
+        balance = 0
+        if not self.value:
+            return balance
+        else:
+            
 
     # the below is all from Cris Ewing; required for this file.
     def get_dot(self):
         """return the tree with root 'self' as a dot graph for visualization"""
-        return "digraph G{\n%s}" % ("" if self.data is None else (
+        return "digraph G{\n%s}" % ("" if self.value is None else (
             "\t%s;\n%s\n" % (
-                self.data,
+                self.value,
                 "\n".join(self._get_dot())
             )
         ))
@@ -69,18 +94,18 @@ class BSTree(object):
     def _get_dot(self):
         """recursively prepare a dot graph entry for this node."""
         if self.left is not None:
-            yield "\t%s -> %s;" % (self.data, self.left.data)
+            yield "\t%s -> %s;" % (self.value, self.left.value)
             for i in self.left._get_dot():
                 yield i
         elif self.right is not None:
             r = random.randint(0, 1e9)
             yield "\tnull%s [shape=point];" % r
-            yield "\t%s -> null%s;" % (self.data, r)
+            yield "\t%s -> null%s;" % (self.value, r)
         if self.right is not None:
-            yield "\t%s -> %s;" % (self.data, self.right.data)
+            yield "\t%s -> %s;" % (self.value, self.right.value)
             for i in self.right._get_dot():
                 yield i
         elif self.left is not None:
             r = random.randint(0, 1e9)
             yield "\tnull%s [shape=point];" % r
-            yield "\t%s -> null%s;" % (self.data, r)
+            yield "\t%s -> null%s;" % (self.value, r)
--- bst_tests.py
+++ bst_tests.py
@@ -1,7 +1,78 @@
 import unittest
-from bst import Node, BSTree
+from bst import BSTree
 
 
 class TestingTheBasics(unittest.TestCase):
     """To make sure the basic parts of this work."""
-    
+    def setUp(self):
+        self.my_bst = BSTree()
+
+    def test_if_the_blind_single_tree_init_works(self):
+        self.assertEqual(self.my_bst.value, None)
+        self.assertEqual(self.my_bst.right, None)
+        self.assertEqual(self.my_bst.left, None)
+
+    def test_the_insert_function(self):
+        self.my_bst.insert(5)
+        self.assertEqual(self.my_bst.value, 5)
+
+    def test_inserting_subtrees_left_and_right(self):
+        self.my_bst.insert(5)
+        self.my_bst.insert(3)
+        self.assertEqual(self.my_bst.left.value, 3)
+        self.my_bst.insert(6)
+        self.assertEqual(self.my_bst.right.value, 6)
+        #now to make sure there's no ghost data hanging around.
+        self.assertIsNone(self.my_bst.right.left)
+        self.assertIsNone(self.my_bst.right.right)
+
+    def test_filling_up_a_tree(self):
+        self.my_bst.insert(5)
+        self.my_bst.insert(3)
+        self.my_bst.insert(6)
+        self.my_bst.insert(8)
+        self.my_bst.insert(11)
+        self.my_bst.insert(2)
+        self.assertEqual(self.my_bst.left.value, 3)
+        self.assertEqual(self.my_bst.left.left.value, 2)
+        self.assertEqual(self.my_bst.right.value, 6)
+        self.assertEqual(self.my_bst.right.right.value, 8)
+        self.assertEqual(self.my_bst.right.right.right.value, 11)
+
+class TestingMyMethods(unittest.TestCase):
+    """Now let's test the methods"""
+    def setUp(self):
+        self.my_bst = BSTree()
+        self.filled_tree = BSTree()
+        self.filled_tree.insert(5)
+        self.filled_tree.insert(3)
+        self.filled_tree.insert(6)
+        self.filled_tree.insert(8)
+        self.filled_tree.insert(11)
+        self.filled_tree.insert(2)
+
+    def test_the_contains_method_on_empty_tree(self):
+        self.assertEqual(self.my_bst.contains(5), False)
+
+    def test_contains_on_tree_that_doesnt_have_the_val(self):
+        self.assertEqual(self.filled_tree.contains(1), False)
+
+    def test_contains_for_values_it_has(self):
+        self.assertEqual(self.filled_tree.contains(8), True)
+        self.assertEqual(self.filled_tree.contains(2), True)
+        self.assertEqual(self.filled_tree.contains(5), True)
+        self.assertEqual(self.filled_tree.contains(11), True)
+        self.assertEqual(self.filled_tree.contains(6), True)
+        self.assertEqual(self.filled_tree.contains(3), True)
+
+    def test_size(self):
+        self.assertEqual(self.my_bst.size(), 0)
+        self.my_bst.insert(1)
+        self.assertEqual(self.my_bst.size(), 1)
+        self.assertEqual(self.filled_tree.size(), 35)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
