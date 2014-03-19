62b67dc53df966da54c38b65348d0161d66f6b16
Luke Petschauer <lpetschauer@gmail.com>
2014-03-19 16:27:50 -0700

    obligatory travis update

--- quicksort.py
+++ quicksort.py
@@ -12,29 +12,32 @@ def quicksort(li):
     greater = [x for x in li if x > pivot]
     equal = [x for x in li if x == pivot]
     results = []
-    less = quicksort(less)  # got this out from rosettacode. was getting
-                            # errors before when I didn't have the assignment.
+    less = quicksort(less)  # have to redefine these vars because otherwise
+                            # everything gets screwed up below.
     greater = quicksort(greater)
     results += less
     results += equal
     results += greater
     return results
 
+
 if __name__ == '__main__':
-    test_list = []
-    time_catcher = []
+    test_random_list = []
+    test_ordered_list = []
 
     for i in range(1000):
-        test_list.append(random.randint(0, 10000))
+        test_random_list.append(random.randint(0, 10000))
 
-    for i in range(500):
-        start_time = time.time()
-        quicksort(test_list)
-        end_time = time.time()
-        time_catcher.append(end_time - start_time)
+    for i in range(1000):
+        test_ordered_list.append(i)
 
-    print "Over 500 iterations of a list of 1,000 random numbers from "\
-    "0-9,999, the longest sort time was", max(time_catcher), "sec, while the "\
-    "shortest time was", min(time_catcher), "sec.\n"\
-    "This gives a small demonstration of the efficacy of using a random value"\
-    " as the pivot."
+    start_time = time.time()
+    quicksort(test_random_list)
+    mid_time = time.time()
+    quicksort(test_ordered_list)
+    end_time = time.time()
+
+    print "Time to sort a random list of 1000 numbers in range(0,9999):"
+    print mid_time - start_time, 'seconds'
+    print "Time to sort a list of 1000 numbers from 0-9999:"
+    print end_time - mid_time, 'seconds'
