6cd0cca9b77570b6a01a29d2be7968e57a329fe5
Luke Petschauer <lpetschauer@gmail.com>
2014-04-06 15:25:04 -0700

    small radix changes

--- radix.py
+++ radix.py
@@ -1,22 +1,20 @@
-
 def radix(li):
     buckets = []  # to hold 0 through 9
-    # ok, here's an uuuuugly way to find out how far to go.
-    ugly_counter = [len(str(i)) for i in li]
-    as_far_as_we_go = max(ugly_counter)
-    # it ain't pretty but it works.
     for i in range(10):
         buckets.append([])
-        # buckets[0] is for 0s, buckets[1] for 1s, &c.
+    # buckets[0] is for 0s, buckets[1] for 1s, &c.
+    # ok, here's an uuuuugly way to find out how far to go.
+    ugly_counter = [len(str(i)) for i in li]
+    as_far_as_we_go = 10**(max(ugly_counter) - 1)
+    # it ain't pretty but it works.
     counter = 1
-    work_list = [num for num in list]
+    work_list = [num for num in li]
     while counter <= as_far_as_we_go:
         for number in work_list:
             buckets[number % counter].append(number)
         empty_catcher = []
         for bucket in buckets:
-            for number in bucket:
-                empty_catcher.extend(number)
+            empty_catcher.extend(bucket)
         work_list = empty_catcher
-        counter += 1
-    return sorted_list
+        counter *= 10
+    return empty_catcher
